e3437cccd0888ca8d4473bca13c928b1ebd51b0b
Abdjulaziz <Abdulazizpriv@gmail.com>
2021-12-08 12:58:55 +0100

    Detail page and styling done, subpage layout done, and a bit styling edited

--- webdev-semester-1/wwwroot/js/site.js
+++ webdev-semester-1/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+document.getElementById("sideNav").addEventListener("click", openNav);
+
