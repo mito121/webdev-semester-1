f0e21142ae9a7f5a677cdf0d47af74680548ceb8
Eilerskov <eilerskov92@gmail.com>
2021-12-10 12:26:43 +0100

    Added UserProfile ViewModel. Joined User and Address from db.

---
+++ webdev-semester-1/ViewModels/UserProfileVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webdev_semester_1.ViewModels
+{
+    public class UserProfileVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AddressId { get; set; }
+
+        public string AddressName { get; set; }
+        public int PostalCode { get; set; }
+        public int CityId { get; set; }
+    }
+}
