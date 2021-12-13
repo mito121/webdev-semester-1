4312814e987e7b4deb37c37b9e2c6d33ee6c3547
Benjamin Porsgaard <benjamin_porsgaard@hotmail.com>
2021-12-13 15:03:39 +0100

    Calendar opdateret

--- webdev-semester-1/wwwroot/js/site.js
+++ webdev-semester-1/wwwroot/js/site.js
@@ -1,19 +1,51 @@
 ﻿if (document.querySelector('#calendar1')) {
+
+
+    let display = "none";
+
     function selectDate(date) {
         $('#calendar1').updateCalendarOptions({
             date: date
         });
-        console.log(calendar.getSelectedDate());
+        console.log(calendar1.getSelectedDate());
+
+        if (display == "none") {
+            display = "block";
+            $(".popUpEditDay").css("display", "flex")
+        } else {
+            display = "none";
+            $('.popUpEditDay').css("display", "none")
+        }
+
+        $('#edit-overlay').css("display", display);
     }
 
-    var defaultConfig = {
+    function calendar2DontSelectDate() {
+        console.log('hej');
+    }
+ 
+
+    var today = new Date();
+    var dd = today.getDate();
+
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
+    var calendar1Config = {
         weekDayLength: 1,
-        date: '08/05/2021',
+        date: today,
         onClickDate: selectDate,
         showYearDropdown: true,
         startOnMonday: true,
     };
 
-    var calendar = $('#calendar1').calendar(defaultConfig);
-    console.log(calendar.getSelectedDate());
+    var calendar2Config = {
+        showAvailabilityButton: false,
+        onClickDate: calendar2DontSelectDate
+    }
+
+    var calendar1 = $('#calendar1').calendar(calendar1Config);
+    /*var calendar2 = $('#calendar2').calendar(calendar2Config);*/
+    console.log(calendar1.getSelectedDate());
 }
